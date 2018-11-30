54132811bf35f6739187693329ad5ee8406ca80c
AngelMartinJimenez <45063649+AngelMartinJimenez@users.noreply.github.com>
2018-11-30 13:21:21 +0100

    sout

--- src/control/main.java
+++ src/control/main.java
@@ -6,8 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		usuario usr = new usuario("angel");
-		System.out.println(usr.toString());
+		System.out.println("Hola angel");
 	}
 
 }
