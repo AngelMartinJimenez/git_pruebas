e35410744a767543a2470644b8f1734a139c0028
AngelMartinJimenez <45063649+AngelMartinJimenez@users.noreply.github.com>
2018-11-30 13:21:52 +0100

    main

--- src/control/main.java
+++ src/control/main.java
@@ -1,10 +1,13 @@
 package control;
 
+import model.usuario;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		usuario usr = new usuario("angel");
+		System.out.println(usr.toString());
 	}
 
 }
